c5c7d3f7ebf8a71d5329cefdb6b6b909c3b6a10a
Mihail Petrov <mihailpetrov1@yahoo.com>
2023-07-29 14:50:05 +0300

    more unittest added

--- SU_Transportation/common/tests.py
+++ SU_Transportation/common/tests.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
 from SU_Transportation.accounts.validators import only_integers
 from SU_Transportation.common.forms import DriverApplicationForm, ApplicationAddressForm
-from SU_Transportation.common.models import DriverApplication
+from SU_Transportation.common.models import DriverApplication, ContactUsModel
+
+UserModel = get_user_model()
 
 
 class IntegerValidator(TestCase):
@@ -71,3 +74,81 @@ class DriverApplicationViewTest(TestCase):
         response = self.client.post(reverse('driver apply'), data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(DriverApplication.objects.count(), 1)
+
+
+class ContactUsViewTest(TestCase):
+    VALID_DATA = {
+        'Name': 'TestCompany',
+        'Email': 'test@test.com',
+        'Phone': '2242245454',
+        'Message': 'test message'
+    }
+    INVALID_DATA = {
+        'Name': 'TestCompany',
+        'Email': 'testtestcom',
+        'Phone': '92242245454',
+        'Message': 'test message'
+    }
+
+    def test_contact_us_view_get(self):
+        response = self.client.get(reverse('contact us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<form')
+        self.assertTemplateUsed(response, 'contact_us_page.html')
+
+    def test_contact_us_view_post_with_valid_data(self):
+        response = self.client.post(reverse('contact us'), ContactUsViewTest.VALID_DATA)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(ContactUsModel.objects.count(), 1)
+
+    def test_contact_us_view_post_with_invalid_data(self):
+        response = self.client.post(reverse('contact us'), ContactUsViewTest.INVALID_DATA)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['form'].is_valid())
+        self.assertEqual(ContactUsModel.objects.count(), 0)
+
+
+class MessageViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = UserModel.objects.create_user(
+            username='TestUser',
+            email='test@test.com',
+            password='Mpp#1983'
+        )
+        self.user.is_staff = True
+        self.login_data = {
+            'username': 'TestUser',
+            'password': 'Mpp#1983',
+        }
+        self.client.post(reverse('contact us'), ContactUsViewTest.VALID_DATA)
+
+    def test_load_massages(self):
+        self.client.login(**self.login_data)
+        message = ContactUsModel.objects.get()
+        response = self.client.get(reverse('messages'))
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, message.Name)
+        self.assertContains(response, message.Email)
+
+    def test_read_message_view(self):
+        self.client.login(**self.login_data)
+        message = ContactUsModel.objects.get()
+        response = self.client.get(reverse('message read', args=[message.pk]))
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, message.Name)
+        self.assertContains(response, message.Email)
+
+    def test_delete_message_view(self):
+        self.client.login(**self.login_data)
+        message = ContactUsModel.objects.get()
+        response = self.client.get(reverse('message delete', args=[message.pk]))
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'delete_message_page.html')
+
+    def test_message_deleted(self):
+        self.client.login(**self.login_data)
+        message = ContactUsModel.objects.get()
+        response = self.client.post(reverse('message delete', args=[message.pk]))
+        self.assertEqual(302, response.status_code)
+        self.assertFalse(ContactUsModel.objects.filter(pk=message.pk).exists())
--- SU_Transportation/freights/tests.py
+++ SU_Transportation/freights/tests.py
@@ -1,3 +1,89 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from SU_Transportation.freights.forms import LoadCreateForm, CompleteLoadForm
+from SU_Transportation.freights.models import LoadCreateModel
+from SU_Transportation.freights.validators import positive_number
+
+UserModel = get_user_model()
+
+class PositiveNumberValidator(TestCase):
+
+    def test_positive_number_whenValidData(self):
+        positive_number('5894240')
+        self.assertTrue(True)
+
+    def test_positive_number_whenInvalidData(self):
+        with self.assertRaises(ValidationError) as context:
+            positive_number('-920342')
+        self.assertIsNotNone(context.exception)
+
+
+class LoadFormsTests(TestCase):
+    CREATE_LOAD_DATA = {
+        'Load_Number': '44978',
+        'Pickup': 'PickupTest',
+        'Pickup_Address': '1 Test str.',
+        'Delivery': 'DeliveryTest',
+        'Delivery_Address': '2 Test str.',
+        'Commodity': 'TestCommodity',
+        'Weight': '42000',
+        'Pickup_Date': '05/05/2023',
+        'Pickup_Time': '08:00',
+        'Delivery_Date': '05/06/2033',
+        'Delivery_Time': '08:00',
+    }
+    COMPLETE_LOAD_DATA = {
+        'Load_Number': '44978',
+        'Load_POD': 'C:\\Users\\moni\\PycharmProjects\\SU_Transportation\\media\\documents\\BOL.jpg'
+    }
+
+    def test_postCreateLoadForm_whenValid(self):
+        form = LoadCreateForm(LoadFormsTests.CREATE_LOAD_DATA)
+        self.assertTrue(form.is_valid())
+
+    def test_postCompleteLoadForm_whenValid(self):
+        form = CompleteLoadForm(LoadFormsTests.COMPLETE_LOAD_DATA)
+        self.assertTrue(form.is_valid())
+
+
+class FreightViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = UserModel.objects.create_user(
+            username='TestUser',
+            email='test@test.com',
+            password='Mpp#1983'
+        )
+        self.user.is_staff = True
+        self.login_data = {
+            'username': 'TestUser',
+            'password': 'Mpp#1983',
+        }
+
+    def test_load_freights(self):
+        self.client.login(**self.login_data)
+        self.client.post(reverse('create load'), LoadFormsTests.CREATE_LOAD_DATA)
+        freight = LoadCreateModel.objects.get()
+        response = self.client.get(reverse('details load'))
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, freight.Load_Number)
+        self.assertContains(response, freight.Pickup)
+
+    def test_create_freight_view(self):
+        self.client.login(**self.login_data)
+        response = self.client.post(reverse('create load'), LoadFormsTests.CREATE_LOAD_DATA)
+        freight = LoadCreateModel.objects.get()
+        self.assertEqual(302, response.status_code)
+        self.assertIsNotNone(freight)
+
+    def test_delete_freight_view(self):
+        self.client.login(**self.login_data)
+        self.client.post(reverse('create load'), LoadFormsTests.CREATE_LOAD_DATA)
+        freight = LoadCreateModel.objects.get()
+        response = self.client.post(reverse('delete load', args=[freight.pk]))
+        self.assertEqual(302, response.status_code)
+        self.assertFalse(LoadCreateModel.objects.filter(pk=freight.pk).exists())
+
